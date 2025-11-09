868c6e3c22ee39ff5f5178b97ccf19f2f1a9d056
Crist Yang <cr1st4ever@outlook.com>
2025-11-09 21:41:56 +0800

    EMQX通信

---
+++ main/esp32/lib/Mqtt/Mqtt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by crist yang on 2025/11/9.
+//
+
+#include "Mqtt.h"
---
+++ main/esp32/lib/Mqtt/Mqtt.h
@@ -0,0 +1,16 @@
+//
+// Created by crist yang on 2025/11/9.
+//
+
+#ifndef ESP32_MQTT_H
+#define ESP32_MQTT_H
+
+
+
+class Mqtt {
+
+};
+
+
+
+#endif //ESP32_MQTT_H
